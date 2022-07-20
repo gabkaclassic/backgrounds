1fd028ddbeec7b54e1bce484189130655e5bcb4f
Errodion <kuzminrd44@gmail.com>
2022-07-20 22:13:03 +0300

    System of notifying was added

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from time import strptime, sleep
 from process import Handler
 from whallpaper_chooser import set_wallpaper
 
+import win10toast, platform, os
+
 FORMAT_TIME = "%H:%M:%S"
+
 
 def check_directory():
     update_config()
@@ -13,12 +16,35 @@ def check_directory():
     save_config(handler.count)
 
 
+def app_exit(message, title):
+    plt = platform.system()
+    if plt == "Darwin":
+        command = '''
+        osascript -e 'display notification "{message}" with title "{title}"'
+        '''
+    elif plt == "Linux":
+        command = f'''
+        notify-send "{title}" "{message}"
+        '''
+    elif plt == "Windows":
+        win10toast.ToastNotifier().show_toast(title, message)
+        return
+    else:
+        return
+    os.system(command)
+
+
 def start():
-    while True:
-        check_directory()
-        time = strptime(Configuration.CONFIG['sleep_time'], FORMAT_TIME)
-        seconds = (time.tm_hour * 60 ** 2) + (time.tm_min * 60) + time.tm_sec
-        sleep(seconds)
+    try:
+        while True:
+            check_directory()
+            time = strptime(Configuration.CONFIG['sleep_time'], FORMAT_TIME)
+            seconds = (time.tm_hour * 60 ** 2) + (time.tm_min * 60) + time.tm_sec
+            sleep(seconds)
+    except Exception as exception:
+        app_exit(f"Running of application was finished with error: {exception}", "Backgrounds")
+    finally:
+        exit(0)
 
 
 if __name__ == '__main__':
--- whallpaper_chooser.py
+++ whallpaper_chooser.py
@@ -1,11 +1,8 @@
 import ctypes
 from random import randint as rand
-from os import remove
 
 
 def set_wallpaper(handler):
     filename = handler.download_file(rand(0, handler.count))
     ctypes.windll.user32.SystemParametersInfoW(20, 0, filename, 0)
-    # remove(filename)
-
 
